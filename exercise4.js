6bfa31083115d836459f972c734f263d501cf12a
Cholland14 <cmholland14@gmail.com>
2025-07-04 18:55:25 +1200

    adding exercise 4

---
+++ exercise4.js
@@ -0,0 +1,11 @@
+The + adds together the values to show whether the 'if' statement is true or not. In this case it is. 
+
+2 + 3 is less than 10
+
+let a = 5, b = 7;
+let result = `${a} + ${b} is `;
+if (a + b < 10) {
+result += 'less than 10';
+} else {
+result += 'greater than 10';
+}
