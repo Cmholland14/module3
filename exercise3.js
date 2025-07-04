1b2601155c475777aefa5b7e69c8f7abd94b243e
Cholland14 <cmholland14@gmail.com>
2025-07-04 18:54:18 +1200

    adding exercise 3

---
+++ exercise3.js
@@ -0,0 +1,9 @@
+if (0) console.log('#1 zero is true') - 0 is considered a false value 
+
+if ("0") console.log('#2 zero is true') - run 
+
+if (null) console.log('null is true') - does not run because null is referencing a non existing object - is nothing
+
+if (-1) console.log('negative is true') - run - minus 1 is negative
+
+if (1) console.log('positive is true') - run  - number 1 is positive 
