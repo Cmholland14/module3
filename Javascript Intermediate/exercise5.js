b2fbaf8df80d2ca8f9c0ef6e533b232fbe0d0fea
Cholland14 <cmholland14@gmail.com>
2025-07-16 11:26:25 +1200

    exercise5b

--- Javascript Intermediate/exercise5.js
+++ Javascript Intermediate/exercise5.js
@@ -3,23 +3,35 @@
 // b.
 
 
-function currencyAddition(float1, float2) {
-const factor = 100;
-const int1 = Math.round(float1 * factor);
-const int2 = Math.round(float2 * factor);
-const sum = int1 + int2;
-return sum / factor;
+function currencyOperation(float1, float2, operation, decimals = 2) {
+    const factor = Math.pow(10, decimals);
+    const int1 = Math.round(float1 * factor);
+    const int2 = Math.round(float2 * factor);
+    let resultInt;
+
+    switch (operation) {
+        case '+':
+            resultInt = int1 + int2;
+            break;
+        case '-':
+            resultInt = int1 - int2;
+            break;
+        case '*':
+            
+            resultInt = Math.round((int1 * int2) / factor);
+            break;
+        case '/':
+            if (int2 === 0) throw new Error("Division by zero");
+            resultInt = Math.round((int1 / int2) * factor);
+            break;
+        default:
+            throw new Error(`Unsupported operation: ${operation}`);
+    }
+
+    return resultInt / factor;
 }
 
-function currencyOperation(float1, float2, operation) {
-const factor = 100;
-const int1 = Math.round(float1 * factor);
-const int2 = Math.round(float2 * factor);
-const sum = int1 + int2;
-return sum / factor;
-}
 
-
-console.log(currencyAddition(0.1, 0.2));
-console.log(currencyOperation(0.1, 0.2, '*'));
-console.log(currencyOperation(0.2, 0.2, '-'));
+console.log(currencyOperation(0.1, 0.2, '+')); 
+console.log(currencyOperation(0.1, 0.2, '*')); 
+console.log(currencyOperation(0.2, 0.2, '-')); 
