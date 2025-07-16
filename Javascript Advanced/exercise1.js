1ba00b0c7b4e4734436e42d080ada1a43deb9f01
Cholland14 <cmholland14@gmail.com>
2025-07-16 12:26:27 +1200

    exercise1b,c

--- Javascript Advanced/exercise1.js
+++ Javascript Advanced/exercise1.js
@@ -16,5 +16,25 @@ let counter2 = makeCounter();
 counter2(); // 1
 counter2(); // 2
 
+//b 
 
-//b
+
+function makeCounter(startFrom =0) {
+  let currentCount = startFrom;
+  return function () {
+    currentCount++;
+    console.log(currentCount);
+    return currentCount;
+  };
+}
+
+//c 
+
+function makeCounter(startFrom =0, incrementBy=1) {
+  let currentCount = startFrom;
+  return function () {
+    currentCount+= incrementBy;
+    console.log(currentCount);
+    return currentCount;
+  };
+}
